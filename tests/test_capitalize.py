d4ca69d5b31ae4287e4289b1b43ad057f447939d
Mikhail Orlov <crazyminer398@gmail.com>
2022-09-25 18:56:47 +0300

    created test for function

---
+++ tests/test_capitalize.py
@@ -0,0 +1,10 @@
+from tests_practice.functions.capitalize import capitalize
+
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
